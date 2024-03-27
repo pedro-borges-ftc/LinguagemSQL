462c74cce24b80624fe9866788e35a6679962ded
peuborg <peuborg.8@gmail.com>
2024-03-27 15:24:00 -0300

    fim aula 08

--- Aula08/ListaExercicio_08.sql
+++ Aula08/ListaExercicio_08.sql
@@ -1,1 +1,44 @@
-/*Escreva uma consulta SQL para 
+/*Escreva uma consulta SQL que retorne o nome do cliente, a descrição da finança, o vencimento da finança e o valor (seja ele positivo para crédito e negativo para débito) para todas as entradas em que o cliente seja você. Se baseie pelo seu nome completo (com letras maiúsculas) para encontrar esses registros e os ordene em ordem de vencimento cronológica e ascendente.*/
+select 
+    c.nome as NomeCliente,
+    descricao as DescriçãoFinança,
+    vencimento as DiaVencimento, 
+    valor * tipolancamento as ValorR$
+FROM financas f join cliente c on (f.codigo = c.codigo)
+where c.nome = 'AEVERTON SANTOS DE OLIVEIRA'
+order by DiaVencimento;
+
+/*Escreva uma consulta SQL que retorne o nome do cliente, a quantidade de finanças e o saldo final no mes de fevereiro, para todas as entradas em que o cliente seja você.*/
+select 
+	/*c.nome as NomeCliente,*/
+    'FULANO DE TALZ' AS NomeCliente,
+    Count(1) as QuantidadeFinanças,
+    sum(valor * tipolancamento) as SaldoEmFevereiroR$
+FROM financas f join cliente c on (f.codigo = c.codigo)
+where c.nome = 'AEVERTON SANTOS DE OLIVEIRA' and vencimento < '20240301'
+group by NomeCliente;
+
+/*Escreva uma consulta SQL que retorne o nome do cliente a descrição da finança, a quantidade de finanças e o somatório agrupado pela descrição da finança, para todas as entradas em que o cliente seja você. */
+select 
+	/*c.nome as NomeCliente,*/
+    'FULANO DE TALZ' AS NomeCliente,
+    descricao as DescriçãoFinança,
+    Count(1) as QuantidadeFinanças,
+    sum(valor * tipolancamento) as SaldoEmFevereiroR$
+FROM financas f join cliente c on (f.codigo = c.codigo)
+where c.nome = 'AEVERTON SANTOS DE OLIVEIRA'
+group by NomeCliente,DescriçãoFinança;
+
+/*Escreva uma consulta SQL que retorne a descrição da finança, o vencimento da finança,  o valor e o saldo do dia para todas as entradas em que o cliente seja você.*/
+select 
+    descricao as DescriçãoFinança,
+    vencimento as DiaVencimento, 
+    valor * tipolancamento as ValorR$, 
+    (valor * tipolancamento + (
+      		select sum(valor * tipolancamento) 
+      		from financas 
+      		where codigo = (select codigo from cliente where nome = 'AEVERTON SANTOS DE OLIVEIRA' ) and vencimento < DiaVencimento ) 
+       							) as SaldoResultante
+FROM financas f join cliente c on (f.codigo = c.codigo)
+where c.nome = 'AEVERTON SANTOS DE OLIVEIRA'
+order by DiaVencimento;
---
+++ Aula08/Script_BD_08.sql
@@ -0,0 +1,155 @@
+---------------------------------------------
+-- CRIAÇÃO DE TABELAS PARA O EXERCÍCIO DA OAT
+---------------------------------------------
+
+/* criando a tabela cliente */
+CREATE table cliente(
+codigo bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+nome VARCHAR(100) NOT NULL,
+nascimento DATE NULL,
+email VARCHAR(100) NULL,
+celular VARCHAR(100) NULL
+);
+
+/* criando a tabela financas */
+CREATE table financas(
+id bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+codigo bigint, 
+descricao VARCHAR(100) NOT NULL,
+cadastro DATE NULL,
+vencimento DATE NOT NULL,
+valor real NULL,
+quitado smallint NULL,
+tipoLancamento smallint NULL
+);
+ALTER TABLE financas ADD CONSTRAINT fk_cliente FOREIGN KEY (codigo) REFERENCES cliente (codigo) ;
+
+/* povoando a tabela cliente */
+INSERT INTO cliente (nome) VALUES 
+('AEVERTON SANTOS DE OLIVEIRA'),
+('ALEX JUNIO SANTOS PACHECO'),
+('ALLAN BRITO BARRETO'),
+('ANDRE LUIZ NASCIMENTO COUTINHO JUNIOR'),
+('ANDRE VALTER MENEZES LEITE'),
+('ARTHUR GALVAO LOUREIRO ARGOLO'),
+('BRUNA ADRIELLY DA SILVA FLORO'),
+('CAIO MEDEIROS FARIAS'),
+('CAIO VICTOR LOPES SERRA'),
+('CARLOS HENRIQUE SANTOS DE CARVALHO'),
+('CLARA GABRYELLEN PAIXAO ADERNO'),
+('DANIEL QUEIROGA SANTANA MARTINS'),
+('DIOGENES SANTANA BISPO DOS SANTOS'),
+('ELISSON BASTOS OLIVEIRA MENEZES JUNIOR'),
+('FELIPE CRUZ SOARES'),
+('FELIPE SOUZA TEIXEIRA DA SILVA'),
+('FERNANDO GUSTAVO B. SANTOS'),
+('FILLIPE CAIRES SILVA ALVES'),
+('FREDSON PEREIRA DE OLIVEIRA'),
+('GABRIEL BORGES LUZ GUEDES'),
+('GABRIEL DE JESUS RIBEIRO'),
+('GABRIEL DE SOUZA NASCIMENTO'),
+('GUILHERME SANTOS DE SANTANA'),
+('GUILHERME SOUZA SANTOS FERREIRA'),
+('GUSTAVO MARINHO DE ANDRADE'),
+('GUSTAVO MEIRA ALMEIDA'),
+('IAN DA SILVA BORGES'),
+('IGOR DOS SANTOS VIEIRA'),
+('IGOR SENA HAGGE'),
+('JHON LUIZ SOUSA SANTOS'),
+('JOAO ANTONIO TRAGINO SANTANA'),
+('JOAO FELIPE SANTOS COSTA'),
+('JOAO GABRIEL ABOBOREIRA RODRIGUES'),
+('JOAO PAULO ROSA BATISTA'),
+('JOAO PEDRO SOUZA DE JESUS'),
+('JOAO ROBERTO SANTOS CARDOSO'),
+('JOAO VICTOR FORTUNO FIGUEIREDO'),
+('JOAO VITOR EVANGELISTA SANTANA'),
+('JONATAN DE SOUZA FERREIRA'),
+('JONATAS ALVES OLIVEIRA NETO'),
+('JONATAS DE JESUS SOUZA'),
+('JOSE HENRIQUE'),
+('KAUA SAMPAIO SILVA'),
+('KAYKY OLIVEIRA NASCIMENTO'),
+('KLEBER LUCAS SANTOS BOMFIM'),
+('LAIO HENRIQUE PEREIRA DA SILVA'),
+('LARISSA CARDOSO MENDES'),
+('LARISSA CARVALHO ALMEIDA'),
+('LEANDRO DA HORA SOUZA'),
+('LUAN GABRIEL RIBEIRO LEITE SILVA'),
+('LUCAS MANOEL GOMES MACIEL'),
+('LUIDE MOURA SNTORNA'),
+('MACIEL LOPES FRANCISCO'),
+('MARCIO VINICIUS DA SILVA SANTOS'),
+('NATAN CORREIA DA SILVA'),
+('PEDRO BOMFIM GOMES ALMEIDA'),
+('PEDRO FELIPE DOS ANJOS SANTOS'),
+('PEDRO HENRIQUE FREITAS COUTO'),
+('PEDRO HENRIQUE MOURA DOS SANTOS'),
+('PEDRO KAUAN SACRAMENTO SOUZA LIMA'),
+('PEDRO YAGO BORGES BARBOSA'),
+('RAPHAEL BARRETO GOMES'),
+('RAYAN SANTOS SILVA'),
+('RONALD LUCAS PEREIRA SANTOS'),
+('SAMUEL SANTOS CESAR'),
+('SAULO VICTOR RIBEIRO PINTO'),
+('TASSIO SANTOS DE ALBUQUERQUE DIAS'),
+('THALLES GABRIEL FERREIRA SANTOS'),
+('VICTOR TADEU ARAUJO AUGUSTO'),
+('VITOR LUIS RAMOS SANTOS')
+; 
+
+/* SP Povoamento Banco */
+CREATE PROCEDURE Inserir_Financas(IN cod INT)
+BEGIN
+	INSERT INTO financas (codigo,descricao,cadastro,vencimento,valor,quitado,tipoLancamento)
+	VALUES 
+	(cod,'emasa',CURRENT_DATE,'20240116',155,0,-1),
+	(cod,'coelba',CURRENT_DATE,'20240110',83.72,0,-1),
+	(cod,'aluguel',CURRENT_DATE,'20240108',155,0,-1),
+	(cod,'salário',CURRENT_DATE,'20240101',1412,1,1),
+	(cod,'mercado',CURRENT_DATE,'20240115',355,0,-1),
+	(cod,'açougue',CURRENT_DATE,'20240115',95,0,-1),
+	(cod,'hortifruti',CURRENT_DATE,'20240115',70,0,-1),
+	(cod,'emasa',CURRENT_DATE,'20240216',155,0,-1),
+	(cod,'coelba',CURRENT_DATE,'20240210',83.72,0,-1),
+	(cod,'aluguel',CURRENT_DATE,'20240208',155,0,-1),
+	(cod,'salário',CURRENT_DATE,'20240201',1412,1,1),
+	(cod,'mercado',CURRENT_DATE,'20240215',355,0,-1),
+	(cod,'açougue',CURRENT_DATE,'20240215',95,0,-1),
+	(cod,'hortifruti',CURRENT_DATE,'20240215',70,0,-1),
+	(cod,'emasa',CURRENT_DATE,'20240316',155,0,-1),
+	(cod,'coelba',CURRENT_DATE,'20240310',83.72,0,-1),
+	(cod,'aluguel',CURRENT_DATE,'20240308',155,0,-1),
+	(cod,'salário',CURRENT_DATE,'20240301',1412,1,1),
+	(cod,'mercado',CURRENT_DATE,'20240315',355,0,-1),
+	(cod,'açougue',CURRENT_DATE,'20240315',95,0,-1),
+	(cod,'hortifruti',CURRENT_DATE,'20240315',70,0,-1),
+	(cod,'emasa',CURRENT_DATE,'20240416',155,0,-1),
+	(cod,'coelba',CURRENT_DATE,'20240410',83.72,0,-1),
+	(cod,'aluguel',CURRENT_DATE,'20240408',155,0,-1),
+	(cod,'salário',CURRENT_DATE,'20240401',1412,0,1),
+	(cod,'mercado',CURRENT_DATE,'20240415',355,0,-1),
+	(cod,'açougue',CURRENT_DATE,'20240420',95,0,-1),
+	(cod,'hortifruti',CURRENT_DATE,'20240423',70,0,-1),
+	(cod,'SALDO ANTERIOR',CURRENT_DATE,'20231231',cod,0,1);
+END;
+
+/* povoando a tabela finanças */
+CALL Inserir_Financas(1);
+SELECT * FROM financas order by id desc;
+
+/* Ajustando saldo anterior */
+SELECT * FROM financas where descricao = 'SALDO ANTERIOR' order by id desc;
+update financas set valor = codigo where descricao = 'SALDO ANTERIOR';
+
+/* saldo de um cliente */
+select 
+    c.nome ,
+    vencimento dia, 
+    valor * tipolancamento , 
+    (valor * tipolancamento + (
+      		select sum(valor * tipolancamento) from financas where codigo = 30 and vencimento < dia ) 
+       							) saldo
+FROM financas f join cliente c on (f.codigo = c.codigo)
+where f.codigo = 30
+order by dia;
